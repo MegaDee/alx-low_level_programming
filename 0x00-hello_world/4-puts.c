3ef3c6453798d9f5b73a1cc241e344dc43a24998
Adedotun Aderibigbe <adedezzy@gmail.com>
2022-07-01 12:31:27 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/* My first C project */
+#include<stdio.h>
+/*
+ function main begins program execution
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+} /* end funtion main */
