ed2c9a0287b465bd31c3bbd2fc9536d93b85170b
Adedotun Aderibigbe <adedezzy@gmail.com>
2022-07-12 16:31:51 -0700

    char

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int super = 1;
+	int lobal = 0;
+	while (super < 1024)
+	{
+		if ((super % 3) == 0 || (super % 5) == 0)
+		{
+			lobal += super;
+		}
+		super++;
+	}
+		printf("%d", lobal);
+		print("\n");
+		return (0);
+}
