d887425e7e1f1002411122d79af0f47e4d0f5053
Adedotun Aderibigbe <adedezzy@gmail.com>
2022-07-13 18:30:12 -0700

    reset

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,14 +8,15 @@
 
 void puts_half(char *str)
 {
-	int i;
+	int i, n, len;
 
+	len = 0;
 	for (i = 0; str[i] != '\0'; i++)
-		;
-	i++;
-	for (i /= 2; str[1] != '\0'; i++)
-	{
+		len++;
+	n = (len / 2);
+	if ((len % 2) == 1)
+		n = ((len + 1) / 2);
+	for (i = n; str[i] != '\0'; i++)
 		_putchar(str[i]);
-	}
 	_putchar('\n');
 }
