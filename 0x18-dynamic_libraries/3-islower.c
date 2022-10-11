69ba1d55b8bb9eb3dd52a45087c73b8257445258
Adedotun Aderibigbe <adedezzy@gmail.com>
2022-10-11 16:50:41 +0100

    updated

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - a function that checks for lowercase character
+ * @c: single let input
+ *
+ * Return: 1 if int is lowercase, 0 if otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
